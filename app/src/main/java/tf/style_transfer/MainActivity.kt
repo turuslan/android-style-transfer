1b5ec070ec66f39dd60216f76cf589901f437865
turuslan <turuslan.devbox@gmail.com>
2024-07-06 11:42:50 +0500

    assets, layout

--- app/src/main/java/tf/style_transfer/MainActivity.kt
+++ app/src/main/java/tf/style_transfer/MainActivity.kt
@@ -1,28 +1,54 @@
 package tf.style_transfer
 
+import android.content.res.AssetManager
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import tf.style_transfer.ui.theme.StyleTransferTheme
+
+fun assetBmp(assets: AssetManager, path: String): ImageBitmap {
+    val s = assets.open(path)
+    val bmp = BitmapFactory.decodeStream(s)
+    s.close()
+    return bmp.asImageBitmap()
+}
+
+class Samples(assets: AssetManager) {
+    val image1 = assetBmp(assets, "image1.jpg")
+    val style1 = assetBmp(assets, "style1.jpg")
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val assets = LocalContext.current.assets
+            val samples = remember { Samples(assets) }
             StyleTransferTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    App(
+                        samples, modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
@@ -30,18 +56,37 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+val TILE = 120.dp
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun App(samples: Samples, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize()) {
+        Image(
+            samples.image1, "", modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        )
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Tile(samples.image1)
+            Tile(samples.style1)
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Tile(bmp: ImageBitmap) {
+    Image(bmp, "", modifier = Modifier.size(TILE))
+}
+
+@Preview(showBackground = true, widthDp = 340, heightDp = 600)
+@Composable
+fun AppPreview() {
+    val samples = Samples(LocalContext.current.assets)
     StyleTransferTheme {
-        Greeting("Android")
+        App(samples)
     }
 }
